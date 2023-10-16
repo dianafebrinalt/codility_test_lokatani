28db898b5f51adde898d958a25d5743569b76778
dianafebrinalt <febrinalt123@gmail.com>
2023-10-16 12:19:47 +0700

    delete code that not needed

--- lib/controller/home_page_controller.dart
+++ lib/controller/home_page_controller.dart
@@ -1,6 +1,5 @@
 import 'package:codility_test_lokatani/model/species_list_model.dart';
 import 'package:codility_test_lokatani/service/home_page_service.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
 
@@ -21,8 +20,6 @@ class HomePageController extends GetxController {
       var products = await HomePageService.fetchProducts();
       if (products != null) {
         productList.value = products.data;
-        var data = productList.where((i) => i.sunlight.contains(Sunlight.FILTERED_SHADE)).toList();
-        print("APA AJA KAH : $data");
       }
     } finally {
       isLoading(false);
